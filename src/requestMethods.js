c2d4010a356269045f230c248983a144a99ca3b9
Anmoltuli1 <anmoltuli36@gmail.com>
2022-03-01 14:27:07 +0530

    api url change

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://ecommerceapireact147.herokuapp.com/api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzBhNGUxY2EwNzk0ZjMxZjhlMjg2NSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTQ4NDI4NCwiZXhwIjoxNjQxNzQzNDg0fQ.10EsOeNgqMS5GrJn5BiBMtZxzHtkkalo5ZmqiazKkO4";
 
 export const publicRequest = axios.create({
